2fece9f411850346ca96a3fea0108d8e0960615b
Firito <yuzhefirito@gmail.com>
2020-01-03 12:05:23 +0800

    [modify] 增加动态换肤核心逻辑；

---
+++ Codes/Dreamland.WPF.DynamicSkin/DynamicSkinModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Dreamland.WPF.DynamicSkin.Annotations;
+
+namespace Dreamland.WPF.DynamicSkin
+{
+    /// <summary>
+    /// 动态皮肤模块
+    /// </summary>
+    internal class DynamicSkinModel : INotifyPropertyChanged
+    {
+        private string _currentSkinName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// 皮肤名称
+        /// </summary>
+        public string CurrentSkinName
+        {
+            get => _currentSkinName;
+            set
+            {
+                _currentSkinName = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ Codes/Dreamland.WPF.DynamicSkin/DynamicSkinService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Dreamland.WPF.DynamicSkin
+{
+    /// <summary>
+    /// 换肤服务
+    /// </summary>
+    public static class DynamicSkinService
+    {
+        static DynamicSkinService()
+        {
+            Application.Current?.Dispatcher?.InvokeAsync(() =>
+            {
+                ResourceDictionary = new ConcurrentDictionary<string, object>();
+            });
+        }
+
+        /// <summary>
+        /// 错误输出
+        /// </summary>
+        public static event EventHandler<ErrorOutputEventArgs> ErrorOutput;
+
+        /// <summary>
+        /// 皮肤Key
+        /// </summary>
+        public static string CurrentSkinName
+        {
+            get => DynamicSkinModel.CurrentSkinName;
+            set => DynamicSkinModel.CurrentSkinName = value;
+        }
+
+        /// <summary>
+        /// 资源字典
+        /// </summary>
+        internal static ConcurrentDictionary<string, object> ResourceDictionary { get; private set; }
+
+        /// <summary>
+        /// 动态皮肤模块
+        /// </summary>
+        internal static DynamicSkinModel DynamicSkinModel = new DynamicSkinModel();
+
+        internal static void RaiseErrorOutput(string errorMessage)
+        {
+            Task.Run(() =>
+            {
+                ErrorOutput?.Invoke(null, new ErrorOutputEventArgs(errorMessage));
+            });
+        }
+    }
+}
